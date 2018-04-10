97ef8aaaa7ac3feafce8b7d609eb0254cf8748ef
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2018-04-10 13:45:44 +0300

    add initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(1.Plus(1));
+        }
+    }
+}
